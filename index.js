4d16f20448dd570cc4800061e4e049d2a6542cdd
Mohammed Asef Hashemi <masefh.fi@gmail.com>
2026-05-18 14:34:25 +0300

    exercises 3.1-3.6

---
+++ index.js
@@ -0,0 +1,86 @@
+const express = require ("express");
+const app = express();
+
+let persons=[
+    {
+        id : "1",
+        name : "Arto Hellas",
+        number : "040-123456"
+    },
+    {
+        id : "2",
+        name : "Ada Lovelace",
+        number : "39-44-5323523"
+    },
+    {
+        id : "3",
+        name : "Dan Abramov",
+        number : "12-43-234245"
+    },
+    {
+        id : "4",
+        name : "Mary Poppendieck",
+        number : "39-23-6423122"
+    }
+];
+
+app.use(express.json());
+
+app.get("/api/persons", (request , response)=>{
+    return response.json(persons)
+});
+app.get("/info", (request, response)=>{
+    const date = new Date()
+    const info = (`<div>
+    <h2>Phonebook has info for ${persons.length} people</h2>
+    <h3>${date}</h3>
+    </div>`)
+    response.send(info)
+})
+app.get("/api/persons/:id", (request, response)=>{
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+    if (!person){
+        return response.status(404).end()
+    }
+    
+    response.json(person)
+    
+})
+app.delete("/api/persons/:id", (request, response) =>{
+    const id = request.params.id;
+    const person = persons.find(person => person.id === id)
+    if (!person){
+        return response.status(404).end()
+    }
+    
+    persons = persons.filter((person=>person.id !== id))
+    response.status(204).end()
+    
+})
+app.post("/api/persons/", (request, response)=>{
+    const randomId = Math.floor(Math.random() * 1000000)
+    const body = request.body
+    const reqName = body.name;
+    const reqNumber = body.number;
+    const checkName = persons.find(person=> person.name === reqName)
+    if(!reqName){
+        return response.status(400).json({error : "name is required"})
+    }
+    else if(!reqNumber){
+        return response.status(400).json({error : "number is required"})
+    }
+    else if(checkName){
+        return response.status(400).json({error:"name should be unique"})
+    }
+    const person = {name : reqName, number : reqNumber, id : String(randomId)}
+    persons = persons.concat(person);
+    response.status(201).json(person)
+})
+
+console.log(persons.map(person=> person))
+const PORT = 3001;
+app.listen(PORT, ()=>{
+    console.log(`server is running on port ${PORT}`);
+    
+});
